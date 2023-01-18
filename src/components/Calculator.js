25c1e3e0243fcbd9c39c75f02799602bd0c99115
collins kiprotich <collinskiprotich@gmail.com>
2023-01-18 18:05:54 +0300

    Fix linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import calculate from '../logic/calculate'
+import calculate from '../logic/calculate';
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       total: null,
       next: null,
-      operation: null
+      operation: null,
     };
   }
 
   clickHandle = (e) => {
     const clickedBtn = e.target.innerText;
-    const Result = calculate(this.state,clickedBtn);
+    const Result = calculate(this.state, clickedBtn);
     this.setState(Result);
   }
+
   render() {
-    const { total, operation, next } = this.state
+    const { total, operation, next } = this.state;
     return (
       <div className="MainBody">
         <div className="result">
@@ -46,7 +47,7 @@ class Calculator extends React.Component {
         <div className="num-symb">
           <button className="buttons" type="button" onClick={this.clickHandle}>1</button>
           <button className="buttons" type="button" onClick={this.clickHandle}>2</button>
-          <button className="buttons" type="button"onClick={this.clickHandle}>3</button>
+          <button className="buttons" type="button" onClick={this.clickHandle}>3</button>
           <button className="buttons operator" type="button" onClick={this.clickHandle}>+</button>
         </div>
         <div className="num-symb">
